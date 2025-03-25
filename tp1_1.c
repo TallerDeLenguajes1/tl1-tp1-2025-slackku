7ec844c2788693bc3e3d09e3d9ab13f79133ca59
slackku <bananuya3@gmail.com>
2025-03-25 16:12:06 -0300

    Correccion escritura de la informacion del puntero

--- tp1_1.c
+++ tp1_1.c
@@ -6,7 +6,7 @@ int main()
 
   int numero = 777;
   int *pNumero = &numero;
-  printf("%p\n", *pNumero); // Direccion de memoria almacenada por el puntero 
+  printf("%p\n", pNumero); // Direccion de memoria almacenada por el puntero 
   printf("%p\n", &numero); // Direccion de memoria del numero
   printf("%p\n", &pNumero); // Direccion de memoria del puntero
   printf("%d", sizeof(numero)); // Tamaño de memoria utilizado por el numero
