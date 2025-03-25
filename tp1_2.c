26402c75e0c998158da8e084111b14cfb7ff176f
slackku <bananuya3@gmail.com>
2025-03-25 16:41:01 -0300

    Desarrollo punto 4 apartado d

--- tp1_2.c
+++ tp1_2.c
@@ -2,6 +2,10 @@
 
 int cuadrado(int numero);
 void cuadradoVoid(int *numero);
+void invertir(int *a, int *b);
+void orden(int *a, int *b);
+void mostrarDirMem(int *num);
+void mostrarInfo(int *num);
 
 int main()
 {
@@ -17,19 +21,53 @@ int main()
     scanf("%d", &numero2);
     printf("Direccion Memoria numero ingresado: %p\n", &numero2);
     cuadradoVoid(&numero2);
-    printf("El cuadrado del numero ingresado es: %d", numero2);
+    printf("El cuadrado del numero ingresado es: %d\n", numero2);
+
+    int numero3, numero4;
+
+    printf("Ingrese un numero:");
+    scanf("%d", &numero3);
+    mostrarDirMem(&numero3);
+    printf("Ingrese un numero:");
+    scanf("%d", &numero4);
+    mostrarDirMem(&numero4);
+
+    invertir(&numero3, &numero4);
+    mostrarInfo(&numero3);
+    mostrarInfo(&numero4);
 
     return 0;
 }
 
+void mostrarDirMem(int *num)
+{
+    printf("Direccion de la variable: %p\n", num);
+}
+
+void mostrarInfo(int *num)
+{
+    printf("En la variable esta el valor: %d\n", *num);
+    mostrarDirMem(num);
+}
+
 int cuadrado(int numero)
 {
-    printf("Ingresando a la funcion... Usando numero ubicado en la de direccion: %p\n", &numero);
+    printf("Ingresando a la funcion...\n");
+    mostrarDirMem(&numero);
     return numero * numero;
 }
 
 void cuadradoVoid(int *numero)
 {
-    printf("Ingresando a la funcion... Usando numero ubicado en la de direccion: %p\n", numero);
+    printf("Ingresando a la funcion...\n");
+    mostrarDirMem(numero);
     *numero *= *numero;
+}
+
+void invertir(int *a, int *b)
+{
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
 }
