d9090a5633d0aa2d7128e090c5e05afed411aa59
slackku <bananuya3@gmail.com>
2025-03-25 16:07:50 -0300

    Desarrollo punto 3.e

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() { 
-    printf("Hola mundo");
-    return 0; 
+int main()
+{
+  printf("Hola mundo\n");
+
+  int numero = 777;
+  int *pNumero = &numero;
+  printf("%p\n", *pNumero); // Direccion de memoria almacenada por el puntero 
+  printf("%p\n", &numero); // Direccion de memoria del numero
+  printf("%p\n", &pNumero); // Direccion de memoria del puntero
+  printf("%d", sizeof(numero)); // Tamaño de memoria utilizado por el numero
+  return 0;
 }
